5f3e253257c6a53d56030f2c8ab80b03e8883a54
Julien Bourgeois <bourgeois.julien.dev@gmail.com>
2022-12-07 16:29:18 +0100

    Day 2

--- Day 2 : Rock Paper Scissors/day2.js
+++ Day 2 : Rock Paper Scissors/day2.js
@@ -6,29 +6,31 @@ require('fs').readFileSync("./input.txt", "utf-8").split(/\r?\n/).forEach((line)
         case "A X":
             round = 1 + 3;
             break;
-        case "B Y":
-            round = 2 + 3;
-            break;
-        case "C Z":
-            round = 3 + 3;
-            break;
         case "A Y":
             round = 2 + 6;
             break;
         case "A Z":
             round = 3 + 0;
             break;
+
         case "B X":
             round = 1 + 0;
+            break;
+        case "B Y":
+            round = 2 + 3;
             break;
         case "B Z":
             round = 3 + 6;
             break;
+
         case "C X":
             round = 1 + 6;
             break;
         case "C Y":
             round = 2 + 0;
+            break;
+        case "C Z":
+            round = 3 + 3;
             break;
     }
     score += round;
